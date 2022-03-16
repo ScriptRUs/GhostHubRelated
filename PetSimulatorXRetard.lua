0e93c9c34653981024c8ff77b37097bf4b526683
ScriptRUs <69376579+ScriptRUs@users.noreply.github.com>
2022-03-15 19:59:48 -0500

    Update PetSimulatorXRetard.lua

--- PetSimulatorXRetard.lua
+++ PetSimulatorXRetard.lua
@@ -3,7 +3,7 @@
         vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
         wait(1)
         vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-
+end)
 gameLoadTime = tick()
 
 if not game:IsLoaded() then
